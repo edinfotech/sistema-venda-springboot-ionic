1c0c154f395e5b9516feb68d4b308d1121d1dab4
Edivanio Vieira <edinfosuporte@gmail.com>
2021-04-18 19:06:41 -0300

    projeto criado

---
+++ src/main/java/com/edivanio/springboot/Ionic/SistemaSpringbootComIonicApplication.java
@@ -0,0 +1,13 @@
+package com.edivanio.springboot.Ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSpringbootComIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSpringbootComIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edivanio/springboot/Ionic/SistemaSpringbootComIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edivanio.springboot.Ionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSpringbootComIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
